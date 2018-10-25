56110c04013dcb1b88873386090fc48ad47a99a7
Noop5574 <44319316+Noop5574@users.noreply.github.com>
2018-10-25 21:53:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,23 @@
-const Discord = require('discord.js');
-const A7MD = new Discord.Client();
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+console.log("Discord Bot By So.?");
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
 
-console.log("BOT ONLINE");
- 
-A7MD.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-مساء الخيّر للذين أجادو حُب الحياة مهما كانت الظروف ..:two_hearts::sparkles:
-نورنا بصوتك يا حلو :sparkling_heart:  https://discord.gg/eMpfSdU ] 
-الدعووة خاصة لك ... [ ${member}  ]
-**`) 
+
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (author) {
+  return author.send(' ** مساء الخيّر للذين أجادو حُب الحياة مهما كانت الظروف ..:two_hearts::sparkles:
+نورنا بصوتك يا حلو :sparkling_heart:  https://discord.gg/eMpfSdU ** ')
+
 }).catch(console.error)
 })
+
 
 client.login(process.env.BOT_TOKEN);
